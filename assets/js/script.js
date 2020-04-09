0e9dea610ceb599c31da35d3a435134b6534bf0a
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-04-09 17:30:38 -0400

    created vars and object, updated js and css files

---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+// create vars 15 questions, 4 answers each = 60 answers total. 
+
+var questions = [
+    {
+        // created an array of objects for each question
+        question: "What is the name of the ship captained by Jean Luc Picard in Star Trek: The Next Generation?",
+        answer: "NCC-1701",
+        choices: [
+            "NCC-1701",
+            "Death Star",
+            "Firefly",
+            "Saturn-V",
+        ]
+    },
+    "What is the name of Data's cat?",
+    "Question 3",
+    "Question 4",
+    "Question 5",
+    "Question 6",
+    "Question 7",
+    "Question 8",
+    "Question 9",
+    "Question 10",
+    "Question 11",
+    "Question 12",
+    "Question 13",
+    "Question 14",
+    "Question 15",
+]
+
+var answers = [
+    "Answer 1",
+    "Answer 2",
+    "Answer 3",
+    "Answer 4",
+    "Answer 5",
+    "Answer 6",
+    "Answer 7",
+    "Answer 8",
+    "Answer 9",
+    "Answer 10",
+    "Answer 11",
+    "Answer 12",
+    "Answer 13",
+    "Answer 14",
+    "Answer 15",
+]
+
+
+// create function for button "make it so" to begin the quiz, switch to the Questions page and beging the countdown timer (displaying the timer)
+
+//create function for "try again" button = to send user back to the start, resetting everything
+
+// create a way for the all 15 questions to loop through while staying on the same page
+
+// create a function for the "make it so" button on the questions page to move to the next set of questions
+
+// create a way for the multiple choice selections to verify if true or false and calculate the score
+
+// create a way for the timer to lose 10 seconds if a wrong answer is submitted
+
+// create a way for the timer to send user to Game Over page only if time = 0 while still in question selection stage (make timer end when all questions answered)
+
+// create a function for the submit button on the enter initials form, to actually submit and store the initials alongside their score. Once clicked, move to a new page displaying the leader board with users initials and score added
+
+// make sure the scoreboard can store everyones entries even when refreshed
+
+// make 
